fb6322eaa6f18a189443991e471897fc4e26d14d
open-mfc <mafacai@vip.qq.com>
2019-09-16 10:57:14 +0800

    打包命令

---
+++ oa5-build.ps1
@@ -0,0 +1,6 @@
+﻿cd GIT2019\txooo-oa5
+git pull
+cd oa.site
+npm run build
+cd ..\OA.WebApi
+dotnet publish -o D:\deploy\188\nginx\c#\oa5.weiapi
---
+++ ocpay-build.ps1
@@ -0,0 +1,6 @@
+﻿cd GIT2019\oa.ocpay
+git pull
+cd oa.ocpay.site
+npm run build
+cd ..\oa.ocpay.webapi
+dotnet publish -o D:\deploy\188\nginx\c#\ocpay.weiapi
